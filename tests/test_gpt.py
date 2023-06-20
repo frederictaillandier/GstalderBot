64018dfda176ed42e748fc26b576b832853d9174
frederictaillandier <maujart@gmail.com>
2023-06-20 06:04:05 -0700

    adding chatgpt api

---
+++ tests/test_gpt.py
@@ -0,0 +1,18 @@
+import sys, json, os
+sys.path.append('../srcs/')
+import pytest
+from srcs.gpt import GPTFluffer
+
+def test_telegram_init():
+    with pytest.raises(Exception):
+        GPTFluffer()
+
+def test_gpt_fluff_no_config():
+    fluffer = GPTFluffer("123")
+    with pytest.raises(Exception):
+        fluffer.fluff("Hello World!")
+
+def test_gpt_fluff_success():
+    config = json.loads(os.environ["GSTALDERCONFIG"])
+    fluffer = GPTFluffer(config["open_ai_key"])
+    assert fluffer.fluff("Hello World!") != None
