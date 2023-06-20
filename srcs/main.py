20666968bdc13d15298d9a5ad723e7580efcdfaa
frederictaillandier <maujart@gmail.com>
2023-06-20 02:21:45 -0700

    adding telegram module and config

--- srcs/main.py
+++ srcs/main.py
@@ -1,5 +1,12 @@
-from return5 import return5
+from telegram import TelegramSender
+import json
+import os
+
+def main():
+    config = json.loads(os.environ["GSTALDERCONFIG"])
+    sender = TelegramSender(config["bot_token"], config["chat_id"])
+    sender.send_message("Hello World!")
+    return 0
 
 if __name__ == "__main__": 
-    print(return5())
-    print ("main")
+    main()
---
+++ tests/test_telegram.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../srcs/')
+import pytest
+import os, json
+from srcs.telegram import TelegramSender
+
+def test_telegram_init():
+    with pytest.raises(Exception) as e_info:
+        TelegramSender()
+
+def test_telegram_send_message_bad_config():
+    sender = TelegramSender("123", "123")
+    assert sender.send_message("Hello World!") == False
+
+def test_telegram_send_message_success():
+    config = json.loads(os.environ["GSTALDERCONFIG"])
+    sender = TelegramSender(config["bot_token"], config["chat_id"])
+    assert sender.send_message("Hello World!") == True
