5390ac4709b4c3728ac3eaa1e5b6663e28dad2da
frederictaillandier <maujart@gmail.com>
2023-06-25 14:08:20 -0700

    feat: separating trash sources in 2 different grabbers

--- srcs/trash_schedule_grabber.py
+++ srcs/trash_schedule_grabber.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 from dateutil.parser import parse
+import calendar
 
 def trim_date(date):
     return {
@@ -11,21 +12,65 @@ def trim_date(date):
 def trim_schedule(schedule):
     return list(map(lambda date: trim_date(date), schedule))
 
-class TrashScheduleGrabber:
-    def __init__(self, url):
-        self.url = url
+class AdliwsilTrashScheduleGrabber:
+    def __init__(self):
+        self.url = "https://adliswil.entsorglos.swiss/backend/widget/calendar-dates/{0}-{1}/"
 
-    def get_schedule(self, date = None, until_date = None):
-        if date is None:
-            date = datetime.datetime.today() + datetime.timedelta(days=1)
-        if until_date is None:
-            until_date = date
-        result = requests.get(self.url)
+    def grab(self, date, until):
+        result = requests.get(self.url.format(date.month, date.year))
         if result.status_code != 200:
             raise Exception("Could not get trash schedule from " + self.url)
-        trimmed_schedule = trim_schedule(result.json()['results']['events'])
-        return list(filter(lambda event: event["date"] >= date and event["date"] <= until_date, trimmed_schedule))
+        return  trim_schedule(list(result.json()['results']['events']))
+
+class WeRecycleTrashScheduleGrabber:
+    def __init__(self):
+        pass
+
+    def grab(self, date, until):
+        return []
+
+
+class TrashScheduleGrabber:
+    def __init__(self):
+        self.raw_grabbers = [AdliwsilTrashScheduleGrabber(), WeRecycleTrashScheduleGrabber()]
+        
+    def get_schedule(self, from_date = None, until_date = None):
+        raw_schedule = []
+       
+        # making sure the dates are set and using tomorrow if not
+        if from_date is None:
+            from_date = datetime.datetime.today() + datetime.timedelta(days=1)
+        if until_date is None:
+            until_date = from_date
+
+        # grab events for all the event grabbers
+        for grabber in self.raw_grabbers:
+            print(grabber.grab)
+            raw_schedule = raw_schedule + grabber.grab(from_date, until_date)
+
+
+        # filter the schedule to only contain the dates we want
+        clean_events = list(filter(lambda event: event["date"] >= from_date and event["date"] <= until_date, raw_schedule))
+
+
+        schedule = {}
+        for event in clean_events:
+            if event["date"] not in schedule:
+                schedule[event["date"]] = []
+            schedule[event["date"]].append(event["waste_type"])
+
+        return schedule
+
+    def get_week_trash_message(self):
+        schedule = self.get_schedule(datetime.datetime.today() + datetime.timedelta(days=1), datetime.datetime.today() + datetime.timedelta(days=8))
+        message = "Hello Food Master, for this week you need to put these trashes in front the house before 7:00am:\n"
+        for date in schedule:
+            message = message + " The {0} on {1}.\n".format(schedule[date], calendar.day_name[date.weekday()])
+        return message 
+
 
 if __name__ == "__main__":
-    grabber = TrashScheduleGrabber("https://adliswil.entsorglos.swiss/backend/widget/calendar-dates/7-2023/")
+    grabber = TrashScheduleGrabber()
     print(grabber.get_schedule( datetime.datetime.today() + datetime.timedelta(days=1), datetime.datetime.today() + datetime.timedelta(days=8)))
+    print(grabber.get_week_trash_message())
+
