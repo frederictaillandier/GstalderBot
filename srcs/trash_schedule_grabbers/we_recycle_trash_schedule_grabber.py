c01cede4dfeb14a08a5052a3f1d664fed2cb400f
frederictaillandier <maujart@gmail.com>
2023-07-02 15:17:24 -0700

    feat: we recycle

--- srcs/trash_schedule_grabbers/we_recycle_trash_schedule_grabber.py
+++ srcs/trash_schedule_grabbers/we_recycle_trash_schedule_grabber.py
@@ -1,6 +1,17 @@
 """Module to fetch the trash schedule from the WeRecycle company."""
+import datetime
 class WeRecycleTrashScheduleGrabber:
     """Class to fetch the trash schedule from the WeRecycle company."""
+ 
+    raw_dates = [
+        { "date": datetime.datetime(2023, 7, 11), "waste_type": 0 },
+        { "date": datetime.datetime(2023, 7, 26), "waste_type": 0 },
+        { "date": datetime.datetime(2023, 8, 11), "waste_type": 0 },
+        { "date": datetime.datetime(2023, 8, 28), "waste_type": 0 },
+        { "date": datetime.datetime(2023, 9, 11), "waste_type": 0 },
+        { "date": datetime.datetime(2023, 9, 26), "waste_type": 0 }
+    ]
+
     def __init__(self):
         pass
 
@@ -14,5 +25,5 @@ class WeRecycleTrashScheduleGrabber:
             { "date": datetime.datetime(2021, 6, 3, 0, 0), "waste_type": [0] },
         ]
         """
-        # Todo: implement
-        return []
+        return list(filter(lambda event: event["date"] >= date
+                                        and event["date"] <= until, self.raw_dates))
