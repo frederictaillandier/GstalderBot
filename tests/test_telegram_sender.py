f92da4b5de8cd76f56b8734ad4e09ee1c84a26cd
frederictaillandier <frederic@auterion.com>
2023-07-31 09:54:38 +0200

    fix: using fixture for hello world test

--- tests/test_telegram_sender.py
+++ tests/test_telegram_sender.py
@@ -6,17 +6,22 @@ import pytest
 from srcs.telegram_sender import TelegramSender
 
 
-def test_telegram_send_message_bad_config():
+@pytest.fixture
+def hello_world_str():
+    return "Hello World!"
+
+
+def test_telegram_send_message_bad_config(hello_world_str):
     """Test that TelegramSender fails with bad config"""
     sender = TelegramSender("123", "123")
     with pytest.raises(HTTPError):
-        sender.send_message("Hello World!")
+        sender.send_message(hello_world_str)
 
 
-def test_telegram_send_message_success():
+def test_telegram_send_message_success(hello_world_str):
     """Test that TelegramSender succeeds with good config"""
     config = json.loads(os.environ["GSTALDERCONFIG"])
     sender = TelegramSender(config["bot_token"], config["global_chat_id"])
-    response = sender.send_message("Hello World!")
+    response = sender.send_message(hello_world_str)
     assert response["ok"] is True
-    assert response["result"]["text"] == "Hello World!"
+    assert response["result"]["text"] == hello_world_str
