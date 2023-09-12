29ee35f236023e1688630b9b21176630898e0ae5
frederictaillandier <frederic@auterion.com>
2023-09-12 09:01:54 +0200

    feat: update we-recycle dates for Q4

--- srcs/trash_schedule_grabbers/we_recycle_trash_schedule_grabber.py
+++ srcs/trash_schedule_grabbers/we_recycle_trash_schedule_grabber.py
@@ -13,6 +13,12 @@ class WeRecycleTrashScheduleGrabber:
         {"date": datetime.datetime(2023, 8, 28), "waste_type": 0},
         {"date": datetime.datetime(2023, 9, 11), "waste_type": 0},
         {"date": datetime.datetime(2023, 9, 26), "waste_type": 0},
+        {"date": datetime.datetime(2023, 10, 10), "waste_type": 0},
+        {"date": datetime.datetime(2023, 10, 26), "waste_type": 0},
+        {"date": datetime.datetime(2023, 11, 11), "waste_type": 0},
+        {"date": datetime.datetime(2023, 11, 27), "waste_type": 0},
+        {"date": datetime.datetime(2023, 12, 8), "waste_type": 0},
+        {"date": datetime.datetime(2023, 12, 19), "waste_type": 0},
     )
 
     def grab(self, date: datetime.datetime, until: datetime.datetime) -> list:
